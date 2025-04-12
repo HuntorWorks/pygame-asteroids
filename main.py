a2aa8a1ce20fd28d152fa41d053182561e1d4ad4
HuntorWorks <rt001a9138@gmail.com>
2025-04-12 16:00:19 +0100

    Implement player and star classes in main.py, enhancing game mechanics with movement and shooting functionality. Set up asset paths and initialize game elements, including player ship and stars. Update game loop to handle sprite updates and rendering. Making use of Sprites and sprite groups

--- main.py
+++ main.py
@@ -1,65 +1,108 @@
 import pygame
+import os
+from random import randint
+
+class Star(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(os.path.join(BASE_PATH, "assets/images/star.png")).convert_alpha()
+        self.rect = self.image.get_frect(center=(randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)))
+    
+    def draw(self, surface):
+        surface.blit(self.image, self.rect)
+
+class PlayerShip(pygame.sprite.Sprite):
+
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(os.path.join(BASE_PATH, "assets/images/player.png")).convert_alpha()
+        self.rect = self.image.get_frect(center=(WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
+        self.input_vector = pygame.Vector2()
+
+        #SHIP PROPERTIES    
+        self.SHIP_SPEED = 300
+        self.SHIP_HEALTH = 100
+        self.SHIP_MAX_HEALTH = 100
+        self.SHIP_LASER_COOLDOWN = 0.5
+        self.SHIP_LASER_SPEED = 12
+        self.SHIP_LASER_DAMAGE = 1
+
+    def move(self, delta_time):
+        keys = pygame.key.get_pressed()
+        self.input_vector.x = int(keys[pygame.K_RIGHT] or keys[pygame.K_d]) - int(keys[pygame.K_LEFT] or keys[pygame.K_a])
+        self.input_vector.y = int(keys[pygame.K_DOWN] or keys[pygame.K_s]) - int(keys[pygame.K_UP] or keys[pygame.K_w ])
+
+        if self.input_vector.magnitude() != 0:
+            self.input_vector = self.input_vector.normalize()
+        self.rect.center += self.input_vector * self.SHIP_SPEED * delta_time
+
+    def shoot(self):
+        keys = pygame.key.get_just_pressed()
+        if keys[pygame.K_SPACE]:
+            print("fire laser")
+
+    def update(self, delta_time):
+        self.move(delta_time)
+        self.shoot()
+
+
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 MAX_FPS = 60
+BASE_PATH = os.path.join(os.getcwd(), "pygame-asteroids")
 
+# ASSETS
+BACKGROUND_SPRITE = os.path.join(BASE_PATH, "assets", "images", "background.png")
+ASTEROID_SPRITE = os.path.join(BASE_PATH, "assets", "images", "meteor.png")
+STAR_SPRITE = os.path.join(BASE_PATH, "assets", "images", "star.png")
+LASER_SPRITE = os.path.join(BASE_PATH, "assets", "images", "laser.png")
+
+
+# INIT SETUP
 pygame.init()
 clock = pygame.time.Clock()
-
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Asteroids")
 running = True
 
-# plain surface
-rect_surface = pygame.Surface((100, 150))
-rect_surface.fill("orange")
-x = 100
+#player
+all_sprites_group = pygame.sprite.Group()
+player = PlayerShip(all_sprites_group)
 
-#img surface
-player_surface = pygame.image.load("assets/images/player.png").convert_alpha()
-player_speed = 0.8
-player_pos_x = 300
-player_pos_y = 350
-move_dir_x = 0
-move_dir_y = 0
+#laser_surface
+laser_surface = pygame.image.load(LASER_SPRITE).convert_alpha()
+laser_rect = laser_surface.get_frect(bottomleft=(20, WINDOW_HEIGHT - 20))
+laser_pos_x = 0
+laser_pos_y = 0
+has_shot = False
+laser_active = False
 
-def get_player_movement_vector():
-    global move_dir_x, move_dir_y
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-        move_dir_x = 1
-        return move_dir_x
-    elif keys[pygame.K_LEFT] or keys[pygame.K_a]:
-        move_dir_x = -1
-        return move_dir_x
-    elif keys[pygame.K_DOWN] or keys[pygame.K_s]:
-        move_dir_y = 1
-        return move_dir_y
-    elif keys[pygame.K_UP] or keys[pygame.K_w]:
-        move_dir_y = -1
-        return move_dir_y
-    else:
-        move_dir_x = 0
-        move_dir_y = 0
-        return move_dir_x, move_dir_y   
+#star_surface
+stars_list = []
+for i in range(20):
+    stars_list.append(Star(all_sprites_group))
+
+#Asteroid_surface
+asteroid_surface = pygame.image.load(ASTEROID_SPRITE).convert_alpha()
+asteroid_rect = asteroid_surface.get_frect(center=(WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
+
 
 while running:
+    delta_time = clock.tick() / 1000
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    display_surface.fill("grey")
-    get_player_movement_vector()
+    # BACKGROUND
+    display_surface.fill("black")
+
+    all_sprites_group.update(delta_time)
+    all_sprites_group.draw(display_surface)
     
-    # Update player position
-    player_pos_x += move_dir_x * player_speed
-    player_pos_y += move_dir_y * player_speed
-    
-    display_surface.blit(player_surface, (player_pos_x, player_pos_y))
-    pygame.display.update()
-        
-    clock.tick(MAX_FPS)
+    pygame.display.update()    
 
 pygame.quit()
+
 
